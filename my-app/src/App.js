4bc94e05ada175fcf73a341a0a56c90dbd11004d
Hwangtaehun <r283@naver.com>
2024-02-03 21:35:23 +0900

    챕처17 컴포넌트 이벤트 만들기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,16 @@
-import React, {Component} from 'react';
-import TOC from './components/TOC';
-import Subject from './components/Subject';
-import Content from './components/Content';
+import React, { Component } from 'react';
+import TOC from "./components/TOC";
+import Content from "./components/Content"
+import Subject from "./components/Subject"
 import './App.css';
 
+
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      mode:'welcome',
+      mode:'read',
+      selected_content_id: 2,
       subject:{title:'WEB', sub:'World Wide Web!'},
       welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
@@ -21,39 +23,43 @@ class App extends Component {
   render() {
     console.log('App render');
     var _title, _desc = null;
-    if(this.state.mode === 'welcome'){
+    if(this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
-    } else if(this.state.mode === 'read'){
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+    } else if(this.state.mode === 'read') {
+      var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
     }
     console.log("render", this);
     return (
-      <div className = "App">
+      <div className="App">
         <Subject 
           title={this.state.subject.title} 
           sub={this.state.subject.sub}
           onChangePage={function () {
-            alert("hi");
+            this.setState({mode:'welcome'});
           }.bind(this)}
         ></Subject>
-        {/* <header>
-          <h1><a href="/" onClick={function(e){
-            console.log("event in", this);
-            e.preventDefault();
-            this.setState({ mode: "welcome" });
-          }.bind(this)}>{this.state.subject.title}</a></h1>
-          {this.state.subject.sub}
-        </header> */}
-        <TOC
-          onChangePage={function (){
-            this.setState({mode: "read"});
-          }.bind(this)} 
-        data={this.state.contents}></TOC>
+        <TOC 
+          onChangePage={function(id) {
+            debugger;
+            this.setState({
+              mode:'read',
+              selected_content_id:Number(id)
+            });
+          }.bind(this)}          
+          data={this.state.contents}></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
-    )
+    );
   }
 }
 
