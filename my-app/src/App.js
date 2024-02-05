72713eb22263bfaf1aec39c4a707c40cb6df0df7
Hwangtaehun <r283@naver.com>
2024-02-05 20:12:56 +0900

    update 구현

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -48,7 +48,7 @@ class App extends Component {
       _article = <CreateContent onSubmit={function(_title, _desc) {
         this.max_content_id = this.max_content_id+1;
         var _contents = Array.from(this.state.contents);
-        _content.push({id:this.max_content_id, title:_title, desc:_desc});
+        _contents.push({id:this.max_content_id, title:_title, desc:_desc});
         this.setState({
           contents:_contents,
           mode:'read',
@@ -57,22 +57,23 @@ class App extends Component {
         console.log(_title, _desc);
       }.bind(this)}></CreateContent>
     } else if(this.state.mode === 'update') {
-      var _content = this.getReadContent();
-      _article = <UpdateContent data={_content} onSubmit={function(_id, _title, _desc) {
-        var _contents = Array.from(this.state.contents);
-        var i = 0;
-        while(i < _content.length){
-          if(_content[i].id === _id){
-            _content[i] = {id:_id, title:_title, desc:_desc};
-            break;
+      _content = this.getReadContent();
+      _article = <UpdateContent data={_content} onSubmit={
+        function(_id, _title, _desc) {
+          var _contents = Array.from(this.state.contents);
+          var i = 0;
+          while(i < _contents.length) {
+            if(_contents[i].id === _id) {
+              _contents[i] = {id:_id, title:_title, desc:_desc};
+              break;
+            }
+            i = i + 1;
           }
-          i = i + 1;
-        }
-        this.setState({
-          contents:_contents,
-          mode:'read'
-        });
-      }.bind(this)}></UpdateContent>
+          this.setState({
+            contents:_contents,
+            mode:'read'
+          });
+        }.bind(this)}></UpdateContent>
     }
     return _article;
   }
