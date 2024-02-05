b6eccf9451c97e1b6f2df7574c29af469b5ffcf0
Hwangtaehun <r283@naver.com>
2024-02-05 11:40:09 +0900

    .....

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,6 +11,7 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props)
+    this.max_content_id = 3;
     this.state = {
       mode:'read',
       selected_content_id: 2,
@@ -44,6 +45,13 @@ class App extends Component {
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else  if(this.state.mode === 'create') {
       _article = <CreateContent onSubmit={function(_title, _desc) {
+        this.max_content_id = this.max_content_id + 1;
+        var _contents = this.state.contents.concat(
+          {id:this.max_content_id, title:_title, desc:_desc}
+        );
+        this.setState({
+          contents:_contents
+        });
         console.log(_title, _desc);
       }.bind(this)}></CreateContent>
     }
