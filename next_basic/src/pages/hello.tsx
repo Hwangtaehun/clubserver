27d1b25c180976e54ee7a070f09e29f98204e30e
Hwangtaehun <r283@naver.com>
2024-02-26 20:46:42 +0900

    getServerSideProps를 제거하기 전

---
+++ next_basic/src/pages/hello.tsx
@@ -0,0 +1,11 @@
+export default function Hello(){
+    console.log(typeof window === 'undefined' ? '서버' : '클라이언트')
+
+    return <>hello</>
+}
+
+export const getServerSideProps = () => {
+    return {
+        props: {},
+    }
+}
