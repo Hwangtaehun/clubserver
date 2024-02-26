024c95b4f59503415b63cec76e7fe8b17322b721
Hwangtaehun <r283@naver.com>
2024-02-26 22:03:21 +0900

    Update _document.tsx

--- next_basic/src/pages/_document.tsx
+++ next_basic/src/pages/_document.tsx
@@ -1,8 +1,9 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Docuemnt, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps, } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
+export default function MyDocument() {
   return (
-    <Html lang="en">
+    <Html lang="ko">
       <Head />
       <body>
         <Main />
@@ -10,4 +11,33 @@ export default function Document() {
       </body>
     </Html>
   );
+}
+
+MyDocument.getInitialProps = async(
+  ctx: DocumentContext,
+): Promise<DocumentInitialProps> => {
+  const sheet = new ServerStyleSheet()
+  const originalRenderPage = ctx.renderPage
+
+  console.log(sheet)
+
+  try{
+    ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+    })
+    
+    const initialProps = await Docuemnt.getInitialProps(ctx)
+    return{
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>
+      ),
+    }
+  }finally{
+    sheet.seal()
+  }
 }
