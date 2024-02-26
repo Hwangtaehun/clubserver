4880467140f27c29dd0aea9835ee349cff728863
Hwangtaehun <r283@naver.com>
2024-02-26 21:50:13 +0900

    업로드

--- next_basic/src/pages/todo/[id].tsx
+++ next_basic/src/pages/todo/[id].tsx
@@ -1,36 +1,41 @@
-import Link from "next/link";
+import Link from 'next/link'
+import { NextPageContext } from 'next'
 
-export default function Todo({ todo }) {
-    return(
-        <>
-            <h1>{todo.title}</h1>
-            <ul>
-                <li>
-                    <Link href="/todo/1">1번</Link>
-                </li>
+export default function Todo({
+  todo,
+}: {
+  todo: { userId: number; id: number; title: string; completed: boolean }
+}) {
+  return (
+    <>
+      <h1>{todo.title}</h1>
+      <ul>
+        <li>
+          <Link href="/todo/1">1번</Link>
+        </li>
 
-                <li>
-                    <Link href="/todo/2">2번</Link>
-                </li>
+        <li>
+          <Link href="/todo/2">2번</Link>
+        </li>
 
-                <li>
-                    <Link href="/todo/3">3번</Link>
-                </li>
-            </ul>
-        </>
-    )
+        <li>
+          <Link href="/todo/3">3번</Link>
+        </li>
+      </ul>
+    </>
+  )
 }
 
-Todo.getInitialProps = async (ctx) => {
-    const {
-        query: { id = '' },
-    } = ctx
-
-    const response = await fetch(
-        `https://jsonplaceholder.typicode.com/todos/${id}`,
-    )
-
-    const result = await response.json()
-    console.log('fetch Complete!')
-    return {todo: result}
+Todo.getInitialProps = async (ctx: NextPageContext) => {
+  const {
+    query: { id = '' },
+    // asPath,
+    // query,
+    // res,
+  } = ctx
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${id}`,
+  )
+  const result = await response.json()
+  return { todo: result }
 }
