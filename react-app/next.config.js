4b500bef0eab2c460464869d243f62cecd9358ca
Hwangtaehun <r283@naver.com>
2024-03-02 19:48:24 +0900

    업로드

---
+++ react-app/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const NextConfig = {
+    reactStrictMode: true,
+    poweredByHeader: false,
+    eslint:{
+        ignoreDuringBuilds: true,
+    },
+}
+
+module.exports = NextConfig
