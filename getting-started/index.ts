e3d5cdf5da75d92252adb5dd57e0337fb9c1c9eb
Hwangtaehun <r283@naver.com>
2024-03-02 20:49:52 +0900

    챕터 4까지 완성

--- getting-started/index.ts
+++ getting-started/index.ts
@@ -2,4 +2,16 @@ function Sum(a:number, b:number) {
     return a + b;
 }
 
+// function sayWord(word :타입'매개변수 타입') :타입'리턴 타입'{
+//     return word;
+// }
+
+function sayWord(word: string) : string{
+    return word;
+}
+
 console.log(Sum(10, 20));
+
+//var 변수: Array<배열의 데이터 타입> = [];, var 변수: 배열의 데이터 타입[] = [];
+//tuple은 var 변수: [데이터 타입, 데이터 타입] = [값, 값];
+//빈값 -> var empty: null = null;, var nothingAssigned: undefinde;
