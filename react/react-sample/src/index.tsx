2bf66bfa8bfe30663d8602159342c5f998bf6b8d
Hwangtaehun <r283@naver.com>
2024-04-18 21:05:59 +0900

    업로드

--- react/react-sample/src/index.tsx
+++ react/react-sample/src/index.tsx
@@ -3,22 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
-import Hello from './components/Hello';
-import Name from './components/Name';
-import Message from './components/Message';
-import Parent from './components/ContainerSample';
-import Page from './components/ContextSample';
+// import Hello from './components/Hello';
+// import Name from './components/Name';
+// import Message from './components/Message';
+// import Parent from './components/ContainerSample';
+// import Page from './components/ContextSample';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Hello />
+    {/* <Hello />
     <Name />
     <Message />
     <Parent />
-    <Page />
+    <Page /> */}
   </React.StrictMode>
 );
 
